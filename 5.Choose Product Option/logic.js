ab72b85f8d4d7ebcaf97a766ebe7a8901bfa1f79
@vannd <vannd@rabiloo.com>
2022-08-01 16:24:18 +0700

    update index cpn

--- 5.Choose Product Option/logic.js
+++ 5.Choose Product Option/logic.js
@@ -1,8 +1,9 @@
-function handleChooseOptionProduct(event) {
+function handleChooseOptionProduct(event, price) {
   for (var i = 0, len = $(".choose-product-option-cpn .product-option-1").length; i < len; i++) {
     $(".choose-product-option-cpn .product-option-1")[i].classList.remove('active')
   }
   event.target.classList.toggle('active')
+  $(".choose-product-option-cpn .price-number span").text(price)
 }
 
 function handleChooseOptionProduct2(event) {
